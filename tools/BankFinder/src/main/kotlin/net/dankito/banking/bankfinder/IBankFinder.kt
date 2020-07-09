d86504323f5e6b016662d79ef8b9fa3764770bf1
dankito <git@dankito.net>
2020-07-09 13:52:59 +0200

    Moved IBankFinder and BankInfo to module BankFinder, so that BankingUiCommon now references BankFinder (TODO: get rid of this direct import)

---
+++ tools/BankFinder/src/main/kotlin/net/dankito/banking/bankfinder/IBankFinder.kt
@@ -0,0 +1,16 @@
+package net.dankito.banking.bankfinder
+
+
+interface IBankFinder {
+
+    fun getBankList(): List<BankInfo>
+
+    fun findBankByBankCode(query: String): List<BankInfo>
+
+    fun findBankByNameBankCodeOrCity(query: String?): List<BankInfo>
+
+    fun findBankByBic(bic: String): BankInfo?
+
+    fun preloadBankList()
+
+}
