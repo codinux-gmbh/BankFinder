3b2af72591189faabc16312cc87426ce968c7ca0
dankito <git@dankito.net>
2023-11-05 15:42:25 +0100

    Removed tag '1.0.0-beta1' and appended '-jvm' to JVM container name

--- BankFinderRest/create-jvm-docker-image.sh
+++ BankFinderRest/create-jvm-docker-image.sh
@@ -1,6 +1,6 @@
 
-../gradlew build
+../gradlew build -x test
 
-docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/bank-finder-api:1.0.0-beta1 .
+docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/bank-finder-api-jvm .
 
-docker push docker.dankito.net/dankito/bank-finder-api:1.0.0-beta1
+docker push docker.dankito.net/dankito/bank-finder-api-jvm
--- BankFinderRest/create-native-docker-image.sh
+++ BankFinderRest/create-native-docker-image.sh
@@ -1,6 +1,6 @@
 
-../gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true
+../gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true -x test
 
-docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/bank-finder-api:1.0.0-beta1 .
+docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/bank-finder-api .
 
-docker push docker.dankito.net/dankito/bank-finder-api:1.0.0-beta1
+docker push docker.dankito.net/dankito/bank-finder-api
