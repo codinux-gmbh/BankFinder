4f35f24fce5fb576b376658fbe42301f624e42e1
dankito <git@dankito.net>
2025-08-23 16:37:56 +0200

    Fixed scripts that they only continue execution if command succeeded

--- BankFinderRest/create-jvm-docker-image.sh
+++ BankFinderRest/create-jvm-docker-image.sh
@@ -1,6 +1,6 @@
 
-../gradlew build -x test
+../gradlew build -x test &&
 
-docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/bank-finder-api-jvm .
+docker build -f src/main/docker/Dockerfile.jvm -t docker.dankito.net/dankito/bank-finder-api-jvm . &&
 
 docker push docker.dankito.net/dankito/bank-finder-api-jvm
--- BankFinderRest/create-native-docker-image.sh
+++ BankFinderRest/create-native-docker-image.sh
@@ -1,6 +1,6 @@
 
-../gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true -x test
+../gradlew build -Dquarkus.package.type=native -Dquarkus.native.container-build=true -x test &&
 
-docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/bank-finder-api .
+docker build -f src/main/docker/Dockerfile.native-micro -t docker.dankito.net/dankito/bank-finder-api . &&
 
 docker push docker.dankito.net/dankito/bank-finder-api
